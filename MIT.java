65e85d223a00b67744810fd8326a56a89394ab89
DhwajRanka <101432915+DhwajRanka@users.noreply.github.com>
2022-08-20 17:39:44 +0530

    Add files via upload

---
+++ MIT.java
@@ -0,0 +1,19 @@
+public class MIT {
+    public static void main(String[] args) {
+        Student s1 = new Student(); 
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+        s1.setName("Dhwaj");  
+        s1.setReg_no(35678);  
+        s1.setBranch("EEE");  
+
+        s2.setName("Priyam");
+        s2.setReg_no(25864);
+        s2.setBranch("CSE");
+
+        s3.setName("Ansh");
+        s3.setReg_no(24569);
+        s3.setBranch("IT");
+    }
+}
